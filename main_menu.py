9b4f615f0733ac3283b2f2e84440962e80246f1f
TheAssassin <47896737+megat69@users.noreply.github.com>
2022-04-11 19:53:43 +0200

    Added a car animation in the garage and smooth texture loading

--- main_menu.py
+++ main_menu.py
@@ -2,226 +2,215 @@ from ursina import *
 import os
 
 class MainMenu(Entity):
-    def __init__(self, car, sand_track, grass_track, garage):
-        super().__init__(
-            parent = camera.ui
-        )
+	def __init__(self, car, sand_track, grass_track, garage):
+		super().__init__(
+			parent = camera.ui
+		)
 
-        self.main_menu = Entity(parent = self, enabled = True)
-        self.maps_menu = Entity(parent = self, enabled = False)
-        self.garage_menu = Entity(parent = self, enabled = False)
-        self.pause_menu = Entity(parent = self, enabled = False)
-        self.car = car
+		self.main_menu = Entity(parent = self, enabled = True)
+		self.maps_menu = Entity(parent = self, enabled = False)
+		self.garage_menu = Entity(parent = self, enabled = False)
+		self.pause_menu = Entity(parent = self, enabled = False)
+		self.car = car
 
-        def start():
-            self.maps_menu.enable()
-            self.main_menu.disable()
+		def start():
+			self.maps_menu.enable()
+			self.main_menu.disable()
 
-        def resume():
-            mouse.locked = True
-            self.pause_menu.disable()
+		def resume():
+			mouse.locked = True
+			self.pause_menu.disable()
 
-        def back():
-            self.maps_menu.disable()
-            self.main_menu.enable()
+		def back():
+			self.maps_menu.disable()
+			self.main_menu.enable()
 
-        def back_garage():
-            self.garage_menu.disable()
-            self.main_menu.enable()
-            camera.rotation = (35, -20, 0)
-            self.car.garage_mode = False
-            self.car.disable()
-            garage.disable()
-            grass_track.enable()
+		def back_garage():
+			self.garage_menu.disable()
+			self.main_menu.enable()
+			camera.rotation = (35, -20, 0)
+			self.car.garage_mode = False
+			self.car.disable()
+			garage.disable()
+			grass_track.enable()
 
-        def respawn():
-            if grass_track.enabled == True:
-                self.car.position = (-80, -30, 15)
-                self.car.rotation = (0, 90, 0)
-            if sand_track.enabled == True:
-                self.car.position = (0, -40, 4)
-                self.car.rotation = (0, 65, 0)
-            self.car.speed = 0
-            self.car.count = 0.0
-            self.car.reset_count = 0.0
-            self.car.timer_running = False
-            self.car.anti_cheat = 1
+		def respawn():
+			if grass_track.enabled == True:
+				self.car.position = (-80, -30, 15)
+				self.car.rotation = (0, 90, 0)
+			if sand_track.enabled == True:
+				self.car.position = (0, -40, 4)
+				self.car.rotation = (0, 65, 0)
+			self.car.speed = 0
+			self.car.count = 0.0
+			self.car.reset_count = 0.0
+			self.car.timer_running = False
+			self.car.anti_cheat = 1
 
-        def main_menu():
-            self.car.position = (0, 0, 4)
-            self.car.disable()
-            self.car.rotation = (0, 65, 0)
-            self.car.speed = 0
-            self.car.count = 0.0
-            self.car.reset_count = 0.0
-            self.car.reset_count_timer.disable()
-            self.car.timer_running = False
-            self.car.anti_cheat = 1
-            self.main_menu.enable()
-            self.pause_menu.disable()
-            sand_track.disable()
-            grass_track.enable()
+		def main_menu():
+			self.car.position = (0, 0, 4)
+			self.car.disable()
+			self.car.rotation = (0, 65, 0)
+			self.car.speed = 0
+			self.car.count = 0.0
+			self.car.reset_count = 0.0
+			self.car.reset_count_timer.disable()
+			self.car.timer_running = False
+			self.car.anti_cheat = 1
+			self.main_menu.enable()
+			self.pause_menu.disable()
+			sand_track.disable()
+			grass_track.enable()
 
-        def sand_track_func():
-            self.car.enable()
-            mouse.locked = True
-            self.maps_menu.disable()
-            self.car.position = (0, 0, 4)
-            self.car.rotation = (0, 65, 0)
-            self.car.reset_count_timer.enable()
-            camera.position = (-80, -30, 15)
-            sand_track.enable()
-            grass_track.disable()
-            
-            sand_track.finish_line.enable()
-            sand_track.boundaries.enable()
-            sand_track.wall1.enable()
-            sand_track.wall2.enable()
-            sand_track.wall3.enable()
-            sand_track.wall4.enable()
-            sand_track.wall_trigger.enable()
+		def sand_track_func():
+			self.car.enable()
+			mouse.locked = True
+			self.maps_menu.disable()
+			self.car.position = (0, 0, 4)
+			self.car.rotation = (0, 65, 0)
+			self.car.reset_count_timer.enable()
+			camera.position = (-80, -30, 15)
+			sand_track.enable()
+			grass_track.disable()
 
-            grass_track.finish_line.disable()
-            grass_track.boundaries.disable()
-            grass_track.wall1.disable()
-            grass_track.wall2.disable()
-            grass_track.wall3.disable()
-            grass_track.wall4.disable()
-            grass_track.wall_trigger.disable()
-            grass_track.wall_trigger_ramp.disable()
+			sand_track.finish_line.enable()
+			sand_track.boundaries.enable()
+			sand_track.wall1.enable()
+			sand_track.wall2.enable()
+			sand_track.wall3.enable()
+			sand_track.wall4.enable()
+			sand_track.wall_trigger.enable()
 
-            path = os.path.dirname(os.path.abspath(__file__))
-            highscore = os.path.join(path, "./highscore/highscore-sandtrack.txt")
+			grass_track.finish_line.disable()
+			grass_track.boundaries.disable()
+			grass_track.wall1.disable()
+			grass_track.wall2.disable()
+			grass_track.wall3.disable()
+			grass_track.wall4.disable()
+			grass_track.wall_trigger.disable()
+			grass_track.wall_trigger_ramp.disable()
 
-            with open(highscore, "r") as hs:
-                self.car.highscore_count = hs.read()
+			path = os.path.dirname(os.path.abspath(__file__))
+			highscore = os.path.join(path, "./highscore/highscore-sandtrack.txt")
 
-            self.car.highscore_count = float(self.car.highscore_count)
+			with open(highscore, "r") as hs:
+				self.car.highscore_count = hs.read()
 
-        def grass_track_func():
-            self.car.enable()
-            mouse.locked = True
-            self.maps_menu.disable()
-            self.car.position = (-80, -30, 15)
-            self.car.rotation = (0, 90, 0)
-            self.car.reset_count_timer.enable()
-            grass_track.enable()
-            sand_track.disable()
+			self.car.highscore_count = float(self.car.highscore_count)
 
-            sand_track.finish_line.disable()
-            sand_track.boundaries.disable()
-            sand_track.wall1.disable()
-            sand_track.wall2.disable()
-            sand_track.wall3.disable()
-            sand_track.wall4.disable()
-            sand_track.wall_trigger.disable()
+		def grass_track_func():
+			self.car.enable()
+			mouse.locked = True
+			self.maps_menu.disable()
+			self.car.position = (-80, -30, 15)
+			self.car.rotation = (0, 90, 0)
+			self.car.reset_count_timer.enable()
+			grass_track.enable()
+			sand_track.disable()
 
-            grass_track.finish_line.enable()
-            grass_track.boundaries.enable()
-            grass_track.wall1.enable()
-            grass_track.wall2.enable()
-            grass_track.wall3.enable()
-            grass_track.wall4.enable()
-            grass_track.wall_trigger.enable()
-            grass_track.wall_trigger_ramp.enable()
+			sand_track.finish_line.disable()
+			sand_track.boundaries.disable()
+			sand_track.wall1.disable()
+			sand_track.wall2.disable()
+			sand_track.wall3.disable()
+			sand_track.wall4.disable()
+			sand_track.wall_trigger.disable()
 
-            path = os.path.dirname(os.path.abspath(__file__))
-            highscore = os.path.join(path, "./highscore/highscore-grasstrack.txt")
+			grass_track.finish_line.enable()
+			grass_track.boundaries.enable()
+			grass_track.wall1.enable()
+			grass_track.wall2.enable()
+			grass_track.wall3.enable()
+			grass_track.wall4.enable()
+			grass_track.wall_trigger.enable()
+			grass_track.wall_trigger_ramp.enable()
 
-            with open(highscore, "r") as hs:
-                self.car.highscore_count = hs.read()
+			path = os.path.dirname(os.path.abspath(__file__))
+			highscore = os.path.join(path, "./highscore/highscore-grasstrack.txt")
 
-            self.car.highscore_count = float(self.car.highscore_count)
-        
-        def garage_button_func():
-            self.garage_menu.enable()
-            self.main_menu.disable()
-            self.car.enable()
-            self.car.garage_mode = True
-            garage.enable()
-            grass_track.disable()
+			with open(highscore, "r") as hs:
+				self.car.highscore_count = hs.read()
 
-        def reset_highscore():
-            if sand_track.enabled == True:
-                path = os.path.dirname(os.path.abspath(__file__))
-                highscore = os.path.join(path, "./highscore/highscore-sandtrack.txt")
+			self.car.highscore_count = float(self.car.highscore_count)
 
-                with open(highscore, "w") as hs:
-                    hs.write(str(0.0))
+		def garage_button_func():
+			self.garage_menu.enable()
+			self.main_menu.disable()
+			self.car.enable()
+			self.car.garage_mode = True
+			garage.enable()
+			grass_track.disable()
 
-                with open(highscore, "r") as hs:
-                    self.car.highscore_count = hs.read()
+		def reset_highscore():
+			if sand_track.enabled == True:
+				path = os.path.dirname(os.path.abspath(__file__))
+				highscore = os.path.join(path, "./highscore/highscore-sandtrack.txt")
 
-                self.car.highscore_count = float(self.car.highscore_count)
-            
-            if grass_track.enabled == True:
-                path = os.path.dirname(os.path.abspath(__file__))
-                highscore = os.path.join(path, "./highscore/highscore-grasstrack.txt")
+				with open(highscore, "w") as hs:
+					hs.write(str(0.0))
 
-                with open(highscore, "w") as hs:
-                    hs.write(str(0.0))
+				with open(highscore, "r") as hs:
+					self.car.highscore_count = hs.read()
 
-                with open(highscore, "r") as hs:
-                    self.car.highscore_count = hs.read()
+				self.car.highscore_count = float(self.car.highscore_count)
 
-                self.car.highscore_count = float(self.car.highscore_count)
+			if grass_track.enabled == True:
+				path = os.path.dirname(os.path.abspath(__file__))
+				highscore = os.path.join(path, "./highscore/highscore-grasstrack.txt")
 
-        def red_car():
-            car.texture = "car-red.png"
-        
-        def blue_car():
-            car.texture = "car-blue.png"
-        
-        def green_car():
-            car.texture = "car-green.png"
-        
-        def orange_car():
-            car.texture = "car-orange.png"
+				with open(highscore, "w") as hs:
+					hs.write(str(0.0))
 
-        def black_car():
-            car.texture = "car-black.png"
+				with open(highscore, "r") as hs:
+					self.car.highscore_count = hs.read()
 
-        def white_car():
-            car.texture = "car-white.png"
+				self.car.highscore_count = float(self.car.highscore_count)
 
-        title = Entity(model = "quad", scale = (0.5, 0.2, 0.2), texture = "rally-logo", parent = self.main_menu, y = 0.3)
+		def change_color(color):
+			"""
+			Changes the car color to the selected color after a small animation.
+			"""
+			car.animate_rotation_y(car.rotation_y + 360, duration=.4, curve=curve.in_out_quad)
+			car.texture = f"car-{color}.png"
 
-        start_button = Button(text = "S t a r t - G a m e", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0.02, parent = self.main_menu)
-        garage_button = Button(text = "G a r a g e", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.1, parent = self.main_menu)
-        quit_button = Button(text = "Q u i t", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.22, parent = self.main_menu)
-        quit_button.on_click = application.quit
-        start_button.on_click = Func(start)
-        garage_button.on_click = Func(garage_button_func)
+		title = Entity(model = "quad", scale = (0.5, 0.2, 0.2), texture = "rally-logo", parent = self.main_menu, y = 0.3)
 
-        sand_track_button = Button(text = "S a n d - T r a c k", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0.3, x = -0.5, parent = self.maps_menu)
-        grass_track_button = Button(text = "G r a s s - T r a c k", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0.3, x = 0, parent = self.maps_menu)
-        back_button = Button(text = "< - B a c k", color = color.gray, scale_y = 0.05, scale_x = 0.2, y = 0.45, x = -0.65, parent = self.maps_menu)
+		start_button = Button(text = "S t a r t - G a m e", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0.02, parent = self.main_menu)
+		garage_button = Button(text = "G a r a g e", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.1, parent = self.main_menu)
+		quit_button = Button(text = "Q u i t", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.22, parent = self.main_menu)
+		quit_button.on_click = application.quit
+		start_button.on_click = Func(start)
+		garage_button.on_click = Func(garage_button_func)
 
-        sand_track_button.on_click = Func(sand_track_func)
-        grass_track_button.on_click = Func(grass_track_func)
-        back_button.on_click = Func(back)
+		sand_track_button = Button(text = "S a n d - T r a c k", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0.3, x = -0.5, parent = self.maps_menu)
+		grass_track_button = Button(text = "G r a s s - T r a c k", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0.3, x = 0, parent = self.maps_menu)
+		back_button = Button(text = "< - B a c k", color = color.gray, scale_y = 0.05, scale_x = 0.2, y = 0.45, x = -0.65, parent = self.maps_menu)
 
-        back_button_garage = Button(text = "< - B a c k", color = color.gray, scale_y = 0.05, scale_x = 0.2, y = 0.45, x = -0.65, parent = self.garage_menu)
-        red_button = Button(color = color.red, scale_y = 0.1, scale_x = 0.15, y = 0.2, x = -0.3, parent = self.garage_menu)
-        blue_button = Button(color = color.cyan, scale_y = 0.1, scale_x = 0.15, y = 0.2, x = 0, parent = self.garage_menu)
-        green_button = Button(color = color.lime, scale_y = 0.1, scale_x = 0.15, y = 0.2, x = 0.3, parent = self.garage_menu)
-        orange_button = Button(color = color.orange, scale_y = 0.1, scale_x = 0.15, y = -0.2, x = -0.3, parent = self.garage_menu)
-        black_button = Button(color = color.black, scale_y = 0.1, scale_x = 0.15, y = -0.2, x = 0, parent = self.garage_menu)
-        white_button = Button(color = color.white, scale_y = 0.1, scale_x = 0.15, y = -0.2, x = 0.3, parent = self.garage_menu)
+		sand_track_button.on_click = Func(sand_track_func)
+		grass_track_button.on_click = Func(grass_track_func)
+		back_button.on_click = Func(back)
 
-        back_button_garage.on_click = Func(back_garage)
-        red_button.on_click = Func(red_car)
-        blue_button.on_click = Func(blue_car)
-        green_button.on_click = Func(green_car)
-        orange_button.on_click = Func(orange_car)
-        black_button.on_click = Func(black_car)
-        white_button.on_click = Func(white_car)
+		back_button_garage = Button(text = "< - B a c k", color = color.gray, scale_y = 0.05, scale_x = 0.2, y = 0.45, x = -0.65, parent = self.garage_menu)
+		red_button = Button(color = color.red, scale_y = 0.1, scale_x = 0.15, y = 0.2, x = -0.3, parent = self.garage_menu)
+		blue_button = Button(color = color.cyan, scale_y = 0.1, scale_x = 0.15, y = 0.2, x = 0, parent = self.garage_menu)
+		green_button = Button(color = color.lime, scale_y = 0.1, scale_x = 0.15, y = 0.2, x = 0.3, parent = self.garage_menu)
+		orange_button = Button(color = color.orange, scale_y = 0.1, scale_x = 0.15, y = -0.2, x = -0.3, parent = self.garage_menu)
+		black_button = Button(color = color.black, scale_y = 0.1, scale_x = 0.15, y = -0.2, x = 0, parent = self.garage_menu)
+		white_button = Button(color = color.white, scale_y = 0.1, scale_x = 0.15, y = -0.2, x = 0.3, parent = self.garage_menu)
 
-        p_resume_button = Button(text = "R e s u m e", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0.23, parent = self.pause_menu)
-        p_respawn_button = Button(text = "R e s p a w n", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0.11, parent = self.pause_menu)
-        p_reset_highsore_button = Button(text = "R e s e t - H i g h s c o r e", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.01, parent = self.pause_menu)
-        p_mainmenu_button = Button(text = "M a i n - M e n u", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.13, parent = self.pause_menu)
-        p_mainmenu_button.on_click = Func(main_menu)
-        p_reset_highsore_button.on_click = Func(reset_highscore)
-        p_respawn_button.on_click = Func(respawn)
-        p_resume_button.on_click = Func(resume)
+		back_button_garage.on_click = Func(back_garage)
+		red_button.on_click = Func(change_color, "red")
+		blue_button.on_click = Func(change_color, "blue")
+		green_button.on_click = Func(change_color, "green")
+		orange_button.on_click = Func(change_color, "orange")
+		black_button.on_click = Func(change_color, "black")
+		white_button.on_click = Func(change_color, "white")
+
+		p_resume_button = Button(text = "R e s u m e", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0.23, parent = self.pause_menu)
+		p_respawn_button = Button(text = "R e s p a w n", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0.11, parent = self.pause_menu)
+		p_reset_highsore_button = Button(text = "R e s e t - H i g h s c o r e", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.01, parent = self.pause_menu)
+		p_mainmenu_button = Button(text = "M a i n - M e n u", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.13, parent = self.pause_menu)
+		p_mainmenu_button.on_click = Func(main_menu)
+		p_reset_highsore_button.on_click = Func(reset_highscore)
+		p_respawn_button.on_click = Func(respawn)
+		p_resume_button.on_click = Func(resume)
